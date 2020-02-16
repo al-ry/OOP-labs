e444f58efb7bb2b7cb1a3b93736d1fa373f23310
al-ry <52191655+al-ry@users.noreply.github.com>
2020-02-17 00:03:43 +0300

    inital commit

---
+++ lab1/flipbyte/flipbyte.cpp
@@ -0,0 +1,101 @@
+﻿#include <iostream>
+#include <string>
+#include <optional>
+
+
+using namespace std;
+const int LENGHT_OF_BYTE = 8;
+const int MAX_VALUE_OF_ONE_BYTE = 255;
+const int ARGUMENTS_COUNT = 2;
+
+
+unsigned int ReverseByte(unsigned int decimalNumber)
+{
+	char byte[LENGHT_OF_BYTE] = "";
+	int resultOfShifting;
+	for (int i = 0; i < LENGHT_OF_BYTE; i++)
+	{
+		resultOfShifting = decimalNumber >> i & 1;
+		byte[i] = resultOfShifting + '0';
+	}
+	decimalNumber = stoi(byte, 0, 2);
+	return decimalNumber;
+}
+bool ParseArguments(int argc, char* argv[], string &arg)
+{
+	if (argc != ARGUMENTS_COUNT)
+	{
+		cout << "Numbers arguments is wrong" << endl;
+		cout << "Usage: flipbyte.exe <Decimal number>" << endl;
+		return false;
+	}
+	arg = argv[1];
+	return true;
+}
+bool CheckIsNumber(const string& arg)
+{
+	int tmp;
+	try
+	{
+		tmp = stoi(arg);
+	}
+	catch (invalid_argument)
+	{
+		cout << "Incorrect Input" << endl;
+		cout << "Enter a decimal number" << endl;
+		return false;
+	}
+	catch (out_of_range)
+	{
+		cout << "Incorrect Input" << endl;
+		cout << "Out of range" << endl;
+		return false;
+	}
+	for (size_t i = 0; i < arg.length(); i++)
+	{
+		if (arg[0] == '-' && i == 0)
+		{
+			continue;
+		}
+		if (!isdigit(arg[i])) 
+		{
+			cout << "Incorrect Input" << endl;
+			cout << "Enter a decimal number" << endl;
+			return false;
+		}
+	}
+	return true;
+}
+int main(int argc, char* argv[])
+{
+	string arg;
+	bool isParsed = ParseArguments(argc, argv, arg);
+
+	if (!isParsed)
+	{
+		return 1;
+	}
+
+
+	unsigned int decimalNumber;
+	bool isNumber = CheckIsNumber(arg);
+
+
+	if (!isNumber)
+	{
+		return 1;
+	}
+	decimalNumber = stoi(arg);
+	
+
+	if (decimalNumber < 0 || decimalNumber > MAX_VALUE_OF_ONE_BYTE)
+	{
+		cout << "Incorrect Input" << endl;
+		cout << "Enter a number in range of one byte [0-255]" << endl;
+		return 1;
+	}
+
+	decimalNumber = ReverseByte(decimalNumber);
+	cout << decimalNumber << endl;
+	return 0;
+}
---
+++ lab1/flipbyte/tests.bat
@@ -0,0 +1,55 @@
+
+set PROGRAM="%~1"
+
+rem При запуске без параметров ожидается ненулевой код возврата
+
+%PROGRAM% > nul
+if NOT ERRORLEVEL 1 goto err
+echo Test 1 passed
+
+rem Передаем несколько аргументов
+
+%PROGRAM% 20 20
+if NOT ERRORLEVEL 1 goto err
+echo Test 2 passed
+
+rem Передаем буквы, а не число
+%PROGRAM% cat
+if NOT ERRORLEVEL 1 goto err
+echo Test 3 passed
+
+%PROGRAM% 23abc
+if NOT ERRORLEVEL 1 goto err
+echo Test 4 passed
+
+%PROGRAM% -23abc
+if NOT ERRORLEVEL 1 goto err
+echo Test 5 passed
+
+rem Граничное значение
+%PROGRAM% 255 > %TEMP%\out.txt
+if ERRORLEVEL 1 goto err
+fc %TEMP%\out.txt tests\edge_value_255.txt
+if ERRORLEVEL 1 goto err
+echo Test 6 passed
+
+rem Граничное значение
+%PROGRAM% 0 > %TEMP%\out.txt
+if ERRORLEVEL 1 goto err
+fc %TEMP%\out.txt tests\edge_value_0.txt
+if ERRORLEVEL 1 goto err
+echo Test 7 passed
+
+rem Реверс байтов в числе 6
+%PROGRAM% 6 > %TEMP%\out.txt
+if ERRORLEVEL 1 goto err
+fc %TEMP%\out.txt tests\flip_6.txt
+if ERRORLEVEL 1 goto err
+echo Test 8 passed
+
+echo All tests passed
+exit  0
+
+:err
+echo Program testing failed
+exit  1
