4cf38f667e6944538a024c6a707d40a0ca2f43e7
al-ry <saska_ryb@mail.ru>
2020-03-09 14:40:33 +0300

    удалил лабу

--- lab2/task1/vector_tests/vectorProcessor.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "stdafx.h"
-
-using namespace std;
-
-vector<double> ReadStream();
-bool ProcessVector(vector<double>& vector);
-void PrintVector(const vector<double>& vector);
