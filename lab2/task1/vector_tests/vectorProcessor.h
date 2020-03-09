dfd35d7e5820a9ef8a70901b5c780dea9b48d5fe
al-ry <saska_ryb@mail.ru>
2020-03-09 14:36:09 +0300

    залил первую первое задание 2 лабы

---
+++ lab2/task1/vector_tests/vectorProcessor.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "stdafx.h"
+
+using namespace std;
+
+vector<double> ReadStream();
+bool ProcessVector(vector<double>& vector);
+void PrintVector(const vector<double>& vector);
